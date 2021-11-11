03971414dbc8b84ea364ca14490691fcf52fc800
19rs103 <k19rs103@st.kyusan-u.ac.jp>
2021-11-12 05:06:56 +0900

    ncmbへデータを保存

--- www/keys.js
+++ www/keys.js
@@ -1,2 +1,4 @@
 // This is a JavaScript file
 
+      var appKey = "da139cece3c8b72a764f3004c41e72c2e570e06baa2df1ae9768b2a6b01ec6b0";
+      var clientKey = "da191d4e7eba57c32d61e159c89af999d934f7a1909dd9281ed5e22c45005c51";
